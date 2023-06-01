a43cf438c5a5354032e2824f95e17ce25d937a95
lihang <1019825699@qq.com>
2023-06-01 12:56:24 +0800

    移动cloud_data增加了GNNS data

---
+++ src/lidar_localization/include/sensor_data/gnss_data.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// #include <Geocentric/LocalCartesi
+
+namespace lidar_localization {
+class GNNSData {
+ public:
+  double time = 0.0;
+  double longtitude = 0.0;
+  double latitude = 0.0;
+  double loccal_E = 0.0;
+  double local_N = 0.0;
+  double local_U = 0.0;
+  int status = 0;
+  int service = 0;
+
+ private:
+  static bool origin_position_inited;
+
+ public:
+  void InitOriginPosition();
+  void UpdateXYZ();
+};
+}  // namespace lidar_localization
