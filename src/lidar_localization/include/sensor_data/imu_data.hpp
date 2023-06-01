0da1a9d469acdf14f74eec39201c16ada78f0baa
lihang <1019825699@qq.com>
2023-06-02 00:12:07 +0800

    add imu_data

---
+++ src/lidar_localization/include/sensor_data/imu_data.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace lidar_localization {
+class ImuData {
+ public:
+  struct LinearAccelation {
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+  };
+
+  struct AngularVelocity {
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+  };
+
+  struct Orientation {
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+    double w = 1.0;
+  };
+
+  double time = 0.0;
+  LinearAccelation linear_acc_;
+  AngularVelocity angular_vel_;
+  Orientation orientation_;
+
+ public:
+  Eigen::Matrix3f getOrientationMatrix() {
+    // clang-format off
+    Eigen::Quaterniond q(orientation_.w, orientation_.x, 
+        orientation_.y, orientation_.z);
+    //
+    // q.toRotationMatrix();
+    Eigen::Matrix3f matrix = q.matrix().cast<float>();
+    return matrix;
+  }
+};
+}  // namespace lidar_localization
