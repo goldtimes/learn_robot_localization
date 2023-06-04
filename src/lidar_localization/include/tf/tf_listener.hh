db4840db1490060556c607165c311969b21d4fc4
lihang <1019825699@qq.com>
2023-06-04 12:26:06 +0800

    ros tf to Eigen变换矩阵

---
+++ src/lidar_localization/include/tf/tf_listener.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <Eigen/Dense>
+#include <string>
+
+namespace lh {
+class TFListener {
+ public:
+  TFListener(ros::NodeHandle& nh, const std::string& base_frame_id,
+             const std::string& child_frame_id);
+  TFListener() = default;
+  bool LookupData(Eigen::Matrix4d& transform_matrix);
+
+ private:
+  bool TransformToMatrix(const tf::StampedTransform& transform, Eigen::Matrix4d& matrix);
+
+ private:
+  ros::NodeHandle nh_;
+  tf::TransformListener listener_;
+  std::string base_frame_id_;
+  std::string child_frame_id_;
+};
+}  // namespace lh
