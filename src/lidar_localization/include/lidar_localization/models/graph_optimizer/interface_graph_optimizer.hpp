ec9212ae20f2a63a38d11a275a9613c69013460e
lihang <1019825699@qq.com>
2023-07-24 08:57:30 +0800

    “抽象的接口类，封装添加节点和调用优化算法的接口”

---
+++ src/lidar_localization/include/lidar_localization/models/graph_optimizer/interface_graph_optimizer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <deque>
+#include <string>
+
+namespace lidar_localization {
+class InterfaceGraphOptimizer {
+ public:
+  virtual ~InterfaceGraphOptimizer() {}
+  //优化
+  virtual bool Optimize() = 0;
+  // 输入输出数据
+  virtual bool GetOptimizedPose(
+      std::deque<Eigen::Matrix4f>& optimized_pose) = 0;
+  virtual int GetNodeNum() = 0;
+  // 添加节点、边、鲁棒核
+  virtual void SetEdgeRobusKernerl(std::string robust_kernel_name,
+                                   double robust_kernel_size) = 0;
+  virtual void AddSe3Node(const Eigen::Isometry3d& pose, bool need_fix) = 0;
+  virtual void AddSe3Edge(int vertex_index1, int vertex_index2,
+                          const Eigen::Isometry3d& relative_pose,
+                          const Eigen::VectorXd& noise) = 0;
+  virtual void AddSe3PriorXYZEdge(int se3_vertex_index,
+                                  const Eigen::Vector3d& xyz,
+                                  Eigen::VectorXd& noise) = 0;
+  virtual void AddSe3PriorQuaternionEdge(int se3_vertex_index,
+                                         const Eigen::Quaterniond& quat,
+                                         Eigen::VectorXd& noise) = 0;
+  // 设置优化参数
+  void SetMaxIterationsNum(int max_iterations_num);
+
+ private:
+ protected:
+  int max_iterations_num_ = 512;
+};
+}  // namespace lidar_localization
