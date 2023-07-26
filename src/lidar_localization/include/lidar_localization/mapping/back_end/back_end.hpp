229403c972cd3d32d443c2a5c1832d2cc0c7cb58
lihang <1019825699@qq.com>
2023-07-27 00:35:18 +0800

    "添加调用g2o的接口"

--- src/lidar_localization/include/lidar_localization/mapping/back_end/back_end.hpp
+++ src/lidar_localization/include/lidar_localization/mapping/back_end/back_end.hpp
@@ -6,56 +6,92 @@
 #ifndef LIDAR_LOCALIZATION_MAPPING_BACK_END_BACK_END_HPP_
 #define LIDAR_LOCALIZATION_MAPPING_BACK_END_BACK_END_HPP_
 
-#include <string>
-#include <deque>
 #include <yaml-cpp/yaml.h>
+#include <deque>
 #include <fstream>
+#include <string>
 
 #include "lidar_localization/sensor_data/cloud_data.hpp"
-#include "lidar_localization/sensor_data/pose_data.hpp"
 #include "lidar_localization/sensor_data/key_frame.hpp"
+#include "lidar_localization/sensor_data/pose_data.hpp"
 
+#include "lidar_localization/models/graph_optimizer/g2o/g2o_graph_optimizer.hpp"
 namespace lidar_localization {
 class BackEnd {
-  public:
-    BackEnd();
+ public:
+  BackEnd();
 
-    bool Update(const CloudData& cloud_data, const PoseData& laser_odom, const PoseData& gnss_pose);
+  bool Update(const CloudData& cloud_data, const PoseData& laser_odom,
+              const PoseData& gnss_pose);
 
-    void GetOptimizedKeyFrames(std::deque<KeyFrame>& key_frames_deque);
-    bool HasNewKeyFrame();
-    bool HasNewOptimized();
-    void GetLatestKeyFrame(KeyFrame& key_frame);
-  
-  private:
-    bool InitWithConfig();
-    bool InitParam(const YAML::Node& config_node);
-    bool InitDataPath(const YAML::Node& config_node);
+  void GetOptimizedKeyFrames(std::deque<KeyFrame>& key_frames_deque);
+  bool HasNewKeyFrame();
+  bool HasNewOptimized();
+  void GetLatestKeyFrame(KeyFrame& key_frame);
 
-    void ResetParam();
-    bool SaveTrajectory(const PoseData& laser_odom, const PoseData& gnss_pose);
-    bool MaybeNewKeyFrame(const CloudData& cloud_data, const PoseData& laser_odom);
-    bool MaybeOptimized();
+  bool ForceOptimize();
 
-  private:
-    std::string key_frames_path_ = "";
-    std::string trajectory_path_ = "";
+ private:
+  bool InitWithConfig();
+  bool InitParam(const YAML::Node& config_node);
+  bool InitDataPath(const YAML::Node& config_node);
+  bool InitGraphOptimizer(const YAML::Node& config_node);
 
-    std::ofstream ground_truth_ofs_;
-    std::ofstream laser_odom_ofs_;
+  void ResetParam();
+  bool SaveTrajectory(const PoseData& laser_odom, const PoseData& gnss_pose);
+  bool MaybeNewKeyFrame(const CloudData& cloud_data,
+                        const PoseData& laser_odom);
+  bool MaybeOptimized();
+  // g2o添加顶点和边
+  bool AddNodeAndEdge(const PoseData& gnss_data);
 
-    float key_frame_distance_ = 2.0;
-    int optimize_step_with_none_ = 100;
-    int optimize_step_with_gnss_ = 100;
-    int optimize_step_with_loop_ = 10;
+ private:
+  std::string key_frames_path_ = "";
+  std::string trajectory_path_ = "";
 
-    bool has_new_key_frame_ = false;
-    bool has_new_optimized_ = false;
+  std::ofstream ground_truth_ofs_;
+  std::ofstream laser_odom_ofs_;
 
-    Eigen::Matrix4f last_key_pose_ = Eigen::Matrix4f::Identity();
-    KeyFrame latest_key_frame_;
-    std::deque<KeyFrame> key_frames_deque_;
+  float key_frame_distance_ = 2.0;
+  int optimize_step_with_none_ = 100;
+  int optimize_step_with_gnss_ = 100;
+  int optimize_step_with_loop_ = 10;
+
+  bool has_new_key_frame_ = false;
+  bool has_new_optimized_ = false;
+
+  Eigen::Matrix4f last_key_pose_ = Eigen::Matrix4f::Identity();
+  KeyFrame latest_key_frame_;
+  std::deque<KeyFrame> key_frames_deque_;
+  //  优化器
+  std::shared_ptr<InterfaceGraphOptimizer> graph_optimizer_ptr_;
+
+  class GraphOptimizerConfig {
+   public:
+    GraphOptimizerConfig() {
+      odom_edge_noise.resize(6);
+      gnss_noise.resize(6);
+      close_loop_noise.resize(6);
+    }
+
+   private:
+    Eigen::VectorXd odom_edge_noise;
+    Eigen::VectorXd gnss_noise;
+    Eigen::VectorXd close_loop_noise;
+    bool use_gnss = true;
+    bool use_loop_close = true;
+
+    int optimize_step_with_key_frame = 100;
+    int optimize_step_with_gnss = 100;
+    int optimize_step_with_loop = 10;
+  };
+
+  GraphOptimizerConfig graph_optimizer_config_;
+
+  int new_gnss_cnt_ = 0;
+  int new_loop_cnt_ = 0;
+  int new_key_frame_cnt_ = 0;
 };
-}
+}  // namespace lidar_localization
 
 #endif
